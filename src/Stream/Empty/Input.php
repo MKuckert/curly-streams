85691079184c723a3b7b347a1203733dc0c742ad
martin@curlybracket.de <martin@curlybracket.de@c6d4e3f6-b50d-11de-880b-11b4e8d67c75>
2009-11-12 20:14:44 +0000

    Added EmptyInputStream and InfiniteInputStream.

---
+++ src/Stream/Empty/Input.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Curly_Stream_Empty_Input
+ * 
+ * An input stream containing no data.
+ * 
+ * @author Martin Kuckert
+ * @copyright Copyright (c) 2009 Martin Kuckert
+ * @license New BSD License
+ * @package Curly.Stream.Empty
+ * @since 11.10.2009
+ */
+class Curly_Stream_Empty_Input implements Curly_Stream_Input {
+	
+	/**
+	 * Checks if more data is available in this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return boolean
+	 */
+	public function available() {
+		return false;
+	}
+	
+	/**
+	 * Reads data out of this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return string
+	 * @param integer Number of elements to read
+	 */
+	public function read($len) {
+		return '';
+	}
+	
+	/**
+	 * Skips data of this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return void
+	 * @param integer Number of elements to skip
+	 */
+	public function skip($len) {
+		// Nothing to do
+	}
+	
+}
---
+++ src/Stream/Infinite/Input.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Curly_Stream_Infinite_Input
+ * 
+ * A stream reading the data of an underlying stream in an infinite loop.
+ * The underlying stream has to implement the {@link Curly_Stream_Seekable} interface.
+ * 
+ * This stream returns always true for each {@method available}-call, except
+ * the underlying stream returns false after seeking back to the first position
+ * of this stream.
+ * 
+ * @author Martin Kuckert
+ * @copyright Copyright (c) 2009 Martin Kuckert
+ * @license New BSD License
+ * @package Curly.Stream.Infinite
+ * @since 12.11.2009
+ */
+class Curly_Stream_Infinite_Input extends Curly_Stream_Capsule_Input {
+	
+	/**
+	 * Constructor
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @param Curly_Stream_Input
+	 */
+	public function __construct(Curly_Stream_Input $stream) {
+		if(!($stream instanceof Curly_Stream_Seekable)) {
+			throw new Curly_Stream_Exception('The underlying input stream has to implement the seekable interface');
+		}
+		parent::__construct($stream);
+	}
+	
+	/**
+	 * Checks if more data is available in this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return boolean
+	 */
+	public function available() {
+		if(!$this->_stream->available()) {
+			$this->_stream->seek(0, Curly_Stream_Seekable::ORIGIN_BEGIN);
+			return $this->_stream->available();
+		}
+		else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Reads data out of this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return string
+	 * @param integer Number of elements to read
+	 */
+	public function read($len) {
+		$data='';
+		$dataLen=0;
+		$seeked=false;
+		
+		do {
+			$readData=$this->_stream->read($len-$dataLen);
+			
+			if($readData=='') {
+				// Don't seek twice to avoid a real infinite loop
+				if($seeked) {
+					break;
+				}
+				
+				$this->_stream->seek(0, Curly_Stream_Seekable::ORIGIN_BEGIN);
+				$seeked=true;
+				continue;
+			}
+			
+			$seeked=false;
+			
+			$data.=$readData;
+			$dataLen=strlen($data);
+		}
+		while($dataLen<$len);
+		
+		return $data;
+	}
+	
+	/**
+	 * Skips data of this stream.
+	 * 
+	 * @throws Curly_Stream_Exception
+	 * @return void
+	 * @param integer Number of elements to skip
+	 */
+	public function skip($len) {
+		$this->read($len);
+	}
+	
+}
